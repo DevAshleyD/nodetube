d8c06752dada96ddc5df822fc899b7ed906f2540
Ashley D <webdevmvp@gmail.com>
2020-10-14 02:39:31 +0000

    changes rtmp

--- copySettingsAndPrivateFiles.js
+++ copySettingsAndPrivateFiles.js
@@ -4,4 +4,4 @@ console.log('Copying settings and private file if necessary');
 
 fs.copySync('.env.settings.sample', '.env.settings', { overwrite: false });
 
-fs.copySync('.env.private.sample', '.env.private', { overwrite: false });
+fs.copySync('.env.private.sample', '.env.private', { overwrite: true });
