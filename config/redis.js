bd06394d4108d419668fceae69939caca6c43933
Ashley D <webdevmvp@gmail.com>
2020-10-14 21:56:42 -0400

    updates redis.js

--- config/redis.js
+++ config/redis.js
@@ -7,7 +7,7 @@ if(process.env.REDIS_URL){
   var client = redis.createClient(process.env.REDIS_URL); // creates a new redisClient
 
 } else {
-  const redisHost = process.env.REDIS_HOST || '0.0.0.0';
+  const redisHost = process.env.REDIS_HOST || '127.0.0.1';
   const redisPort = process.env.REDIS_PORT || 6379;
   const redisPassword =  process.env.REDIS_PASSWORD || '';
 
@@ -17,7 +17,7 @@ if(process.env.REDIS_URL){
   };
 
   if(process.env.NODE_ENV == 'production'){
-    options.password = redisPassword;
+    options.password = null;
   }
 
   console.log(`CONNECTING TO REDIS, HOST: ${redisHost}, PORT: ${redisPort}\n`);
